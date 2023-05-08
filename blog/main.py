78ebec602e8afbbdacbccc0a31e151e17249d639
Sushmita Sharangpani <103027497+sushmitaCRM03@users.noreply.github.com>
2023-05-08 23:28:28 +0530

    dummy commit

--- blog/main.py
+++ blog/main.py
@@ -9,3 +9,9 @@ app = FastAPI()
 @app.post('/blog')
 def create(request: schemas.Blog):
     return {'data': f'Blog is created with title as {request.title}'}
+
+# dummy test for pr
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
