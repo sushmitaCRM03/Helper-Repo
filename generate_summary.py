b330234051102ee4728aa29d7974ee11ac8f5fd9
Sushmita Sharangpani <103027497+sushmitaCRM03@users.noreply.github.com>
2023-05-09 00:02:48 +0530

    changes

--- generate_summary.py
+++ generate_summary.py
@@ -1,13 +1,30 @@
 import argparse
+from github import Github
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--token', type=str, help='GitHub token')
 parser.add_argument('--repo', type=str, help='GitHub repository name')
 parser.add_argument('--branch', type=str, help='GitHub branch name')
+parser.add_argument('--pr', type=int, help='GitHub pull request number')
 args = parser.parse_args()
 
-token = args.token
-repo = args.repo
-branch = args.branch
+# Initialize PyGithub client with the token
+g = Github(args.token)
 
-# Use the token, repo, and branch variables to fetch the PR code and generate the summary
+# Get the repository object
+repo = g.get_repo(args.repo)
+
+# Get the pull request object
+pr = repo.get_pull(args.pr)
+
+# Get the head branch of the pull request
+head_branch = pr.head.ref
+
+# Get the files changed in the pull request
+files = pr.get_files()
+
+# Print the contents of each file
+for file in files:
+    contents = file.decoded_content.decode('utf-8')
+    print(f'Contents of {file.filename}:')
+    print(contents)
