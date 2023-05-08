566f39512f1f5a394e5437af6ec8efecb5be332d
Sushmita Sharangpani <103027497+sushmitaCRM03@users.noreply.github.com>
2023-05-08 23:42:49 +0530

    changes

--- generate_summary.py
+++ generate_summary.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+import requests
+import os
+
+repo_name = os.environ['GITHUB_REPOSITORY']
+headers = {'Authorization': f'token {os.environ["GITHUB_TOKEN"]}'}
+url = f'https://api.github.com/repos/{repo_name}'
+response = requests.get(url, headers=headers)
