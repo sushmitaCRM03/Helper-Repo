9d23585bca94e425337f1cf824d1bf945e0ff513
Sushmita Sharangpani <103027497+sushmitaCRM03@users.noreply.github.com>
2023-05-08 23:50:56 +0530

    push

--- generate_summary.py
+++ generate_summary.py
@@ -1,7 +1,13 @@
-import requests
-import os
+import argparse
 
-repo_name = os.environ['GITHUB_REPOSITORY']
-headers = {'Authorization': f'token {os.environ["GITHUB_TOKEN"]}'}
-url = f'https://api.github.com/repos/{repo_name}'
-response = requests.get(url, headers=headers)
+parser = argparse.ArgumentParser()
+parser.add_argument('--token', type=str, help='GitHub token')
+parser.add_argument('--repo', type=str, help='GitHub repository name')
+parser.add_argument('--branch', type=str, help='GitHub branch name')
+args = parser.parse_args()
+
+token = args.token
+repo = args.repo
+branch = args.branch
+
+# Use the token, repo, and branch variables to fetch the PR code and generate the summary
